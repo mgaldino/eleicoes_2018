a39eda06d48f700fb1cc88f88de1f7ad67a10703
silvadenisson <denisson@denissonsilva.com>
2018-10-11 21:40:38 -0300

    down

---
+++ download_dados_urna.R
@@ -0,0 +1,16 @@
+# donwload dados urnas
+
+# abrindo pacotes
+library(rvest)
+library(stringr)
+
+url_base <- "http://agencia.tse.jus.br/estatistica/sead/eleicoes/eleicoes2018/buweb/"
+
+link <- read_html(url_base) %>% html_nodes(xpath = "//a") %>% html_text() %>% .[seq(6,58, 2)]
+
+arquivo <- link[str_detect("MG")]
+
+download.file(paste0(url_base, arquivo), arquivo)
+
+unzip(arquivo)
+
